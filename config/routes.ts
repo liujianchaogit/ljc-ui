b01b78352cd281833e180702520bf58c7ba240e5
liujianchao <102093174@qq.com>
2021-01-12 21:09:07 +0800

    by ljc

---
+++ config/routes.ts
@@ -0,0 +1,31 @@
+﻿export default [
+  // {
+  //   path: '/user',
+  //   layout: false,
+  //   routes: [
+  //     {
+  //       path: '/user',
+  //       routes: [
+  //         {
+  //           name: 'login',
+  //           path: '/user/login',
+  //           component: './User/login',
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
+  // {
+  //   path: '/welcome',
+  //   name: 'welcome',
+  //   icon: 'smile',
+  //   component: './Welcome',
+  // },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    component: './404',
+  },
+];
