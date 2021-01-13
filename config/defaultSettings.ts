99b55eeb9e918b9b0fe98191e78899fa373cbea5
liujianchao <102093174@qq.com>
2021-01-13 22:56:02 +0800

    by ljc

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -4,15 +4,15 @@ const Settings: LayoutSettings & {
   pwa?: boolean;
   logo?: string;
 } = {
-  navTheme: 'light',
+  navTheme: 'dark',
   // 拂晓蓝
   primaryColor: '#1890ff',
-  layout: 'mix',
+  layout: 'side',
   contentWidth: 'Fluid',
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
+  title: 'LJC Design Pro',
   pwa: false,
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   iconfontUrl: '//at.alicdn.com/t/font_2325568_q3odw9sgj7.js',
--- src/access.ts
+++ src/access.ts
@@ -2,8 +2,8 @@
 export default function access(initialState: { currentUser?: API.CurrentUser | undefined }) {
   const { currentUser } = initialState || {};
   return {
-    userRouteFilter: currentUser?.permissions?.indexOf('sys:user'),
-    roleRouteFilter: currentUser?.permissions?.indexOf('sys:role'),
-    menuRouteFilter: false
+    userRouteFilter: currentUser?.permissions && currentUser.permissions.indexOf('sys:user') > -1,
+    roleRouteFilter: currentUser?.permissions && currentUser.permissions.indexOf('sys:role') > -1,
+    menuRouteFilter: currentUser?.permissions && currentUser.permissions.indexOf('sys:menu') > -1
   };
 }
