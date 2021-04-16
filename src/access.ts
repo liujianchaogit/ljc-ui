c7701508a95f4a95f57569e5b73c7f59066abddb
liujianchao <102093174@qq.com>
2021-04-16 15:24:36 +0800

    by ljc

--- src/access.ts
+++ src/access.ts
@@ -3,9 +3,9 @@ import { IRoute } from 'umi';
 export default function access(initialState: { currentUser?: API.CurrentUser }) {
   const { currentUser } = initialState;
   return {
-    userRouteFilter: (route: IRoute) => currentUser?.permissions?.includes(route.name),
-    roleRouteFilter: (route: IRoute) => currentUser?.permissions?.includes(route.name),
-    menuRouteFilter: (route: IRoute) => currentUser?.permissions?.includes(route.name),
-    fcwRouteFilter: (route: IRoute) => currentUser?.permissions?.includes(route.name)
+    userRouteFilter: (route: IRoute) => currentUser?.access?.includes(route.name),
+    roleRouteFilter: (route: IRoute) => currentUser?.access?.includes(route.name),
+    menuRouteFilter: (route: IRoute) => currentUser?.access?.includes(route.name),
+    fcwRouteFilter: (route: IRoute) => currentUser?.access?.includes(route.name)
   }
 }
